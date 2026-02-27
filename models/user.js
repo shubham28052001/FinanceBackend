553ef2fbced7335f99428ab11f3fe9f842b76dbe
Shubham Sanodiya <shubham28052001@gmail.com>
2026-02-27 22:37:37 +0530

    "user Registration"

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
 
 const userSchema = new mongoose.Schema(
   {
@@ -19,33 +20,37 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password is required"],
       minlength: 6,
+      select: false
     },
     role: {
       type: String,
       enum: ["user", "admin"],
       default: "user",
     },
+    refreshToken: {
+      type: String,
+      select: false
+    },
   },
   { timestamps: true }
 );
 
+userSchema.statics.hashPassword = async function (password) {
+  const salt = await bcrypt.genSalt(12);
+  return await bcrypt.hash(password, salt);
+};
 
-// 🔐 Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-
-// 🔎 Compare password method (for login)
-userSchema.methods.comparePassword = async function (enteredPassword) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_ACCESS_KEY, { expiresIn: "15m" })
+}
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_REFRESH_KEY, { expiresIn: "7d" })
+}
 
 
 module.exports = mongoose.model("User", userSchema);
